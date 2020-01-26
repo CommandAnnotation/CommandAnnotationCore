fb32c102ed0ac13b0e6992d79ff6dfd70e4641d3
Skywolf is not Wolf <juni2484ok@naver.com>
2020-01-26 10:12:08 +0900

    Updated structure

--- src/skywolf46/CommandAnnotation/v1_1/Annotations/DefaultCommand.java
+++
@@ -1,9 +0,0 @@
-package skywolf46.CommandAnnotation.v1_1.Annotations;
-
-public @interface DefaultCommand {
-    String command();
-    Class<?>[] requireParameter() default {};
-    Class<?>[] acceptableParameter() default {};
-    boolean fallbackOnSubCommandNotExist() default true;
-
-}
